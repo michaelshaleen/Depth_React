3f9360c499d3f5572d53aa15dc4f5e1ea085094e
michaelshaleen <shaleenmichael@yahoo.com>
2025-07-29 06:54:58 -0500

    filling inputs

--- components/RequestForm.tsx
+++ components/RequestForm.tsx
@@ -7,9 +7,20 @@ function RequestForm({ title }: { title: string }) {
 export default function MyForm() {
   return (
     <fieldset>
+      <h2>Appointment Request</h2>
       <input 
         type="name"
-        placeholder="name"></input>
+        placeholder="name"></input><br/>
+        <input 
+        type="phone number"
+        placeholder="phone number"></input><br/>
+        <input 
+        type="date"
+        ></input>
+        <input 
+        type="message"
+        placeholder="What can we help you with?"
+        ></input>
     </fieldset>
   );
 }
